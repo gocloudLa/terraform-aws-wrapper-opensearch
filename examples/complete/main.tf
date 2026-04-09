844eb22ec68a5385ab7fdcdfdc6131ae570bf901
Guido Montano <gmontano@gocloud.la>
2026-04-09 17:45:17 -0300

    fix: update example/complete

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -6,37 +6,14 @@ module "wrapper_opensearch" {
 
   opensearch_parameters = {
     example = {
-      advanced_options = {
-        "rest.action.multi.allow_explicit_index" = "true"
-      }
 
-      advanced_security_options = {
-        enabled                        = false
-        anonymous_auth_enabled         = true
-        internal_user_database_enabled = true
+      engine_version = "OpenSearch_2.13"
 
-        master_user_options = {
-          master_user_name     = "example"
-          master_user_password = "Barbarbarbar1!"
-        }
-      }
+      domain_name = lower(local.common_name_prefix)
 
-      auto_tune_options = {
-        desired_state = "ENABLED"
-
-        maintenance_schedule = [
-          {
-            start_at                       = "2028-05-13T07:44:12Z"
-            cron_expression_for_recurrence = "cron(0 0 ? * 1 *)"
-            duration = {
-              value = "2"
-              unit  = "HOURS"
-            }
-          }
-        ]
-
-        rollback_on_disable = "NO_ROLLBACK"
-      }
+      # vpc_options = {
+      #   subnet_ids = data.aws_subnets.this.ids
+      # }
 
       cluster_config = {
         instance_count           = 3
@@ -61,13 +38,6 @@ module "wrapper_opensearch" {
         zone_awareness_enabled = true
       }
 
-      domain_endpoint_options = {
-        enforce_https       = true
-        tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
-      }
-
-      domain_name = lower(local.common_name_prefix)
-
       ebs_options = {
         ebs_enabled = true
         iops        = 3000
@@ -76,11 +46,46 @@ module "wrapper_opensearch" {
         volume_size = 20
       }
 
+      auto_tune_options = {
+        desired_state = "ENABLED"
+
+        maintenance_schedule = [
+          {
+            start_at                       = "2028-05-13T07:44:12Z"
+            cron_expression_for_recurrence = "cron(0 0 ? * 1 *)"
+            duration = {
+              value = "2"
+              unit  = "HOURS"
+            }
+          }
+        ]
+
+        rollback_on_disable = "NO_ROLLBACK"
+      }
+
+      advanced_options = {
+        "rest.action.multi.allow_explicit_index" = "true"
+      }
+
+      advanced_security_options = {
+        enabled                        = false
+        anonymous_auth_enabled         = true
+        internal_user_database_enabled = true
+
+        master_user_options = {
+          master_user_name     = "example"
+          master_user_password = "Barbarbarbar1!"
+        }
+      }
+
+      domain_endpoint_options = {
+        enforce_https       = true
+        tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
+      }
+
       encrypt_at_rest = {
         enabled = true
       }
-
-      engine_version = "OpenSearch_2.13"
 
       log_publishing_options = [
         { log_type = "INDEX_SLOW_LOGS" },
@@ -96,10 +101,6 @@ module "wrapper_opensearch" {
       software_update_options = {
         auto_software_update_enabled = true
       }
-
-      # vpc_options = {
-      #   subnet_ids = data.aws_subnets.this.ids
-      # }
 
       # VPC endpoint
       # vpc_endpoints = {
